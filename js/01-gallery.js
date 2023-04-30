ac37d875313c3738ece132b44c8b56667ed04632
Yura <113457886+Yura-Platonov@users.noreply.github.com>
2023-04-30 16:41:18 +0300

    made galary

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,54 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+    gallery: document.querySelector('.gallery'),
+}
+
+//генерація розмітки
+const galleryMarkup = createGalleryCardsMarkup(galleryItems);
+refs.gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryCardsMarkup(imgs) {
+    return imgs.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+            <a class="gallery__link" onclick="event.preventDefault()" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>`;
+    }).join('');
+};
+
+//клік по фото
+refs.gallery.addEventListener('click', clickOnPhoto);
+
+function clickOnPhoto(e) {
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    } 
+    
+    const createModal = basicLightbox.create(`
+        <img src="${e.target.dataset.source}" width="800" height="600">
+    `, {
+        onClose: (createModal) => {
+            window.removeEventListener('keydown', createModalOnEscKeyPressModal);
+        }
+    })
+    const createModalOnEscKeyPressModal = onEscKeyPress.bind(createModal);
+    window.addEventListener('keydown', createModalOnEscKeyPressModal);
+    createModal.show();
+}
+
+function onEscKeyPress(e) {
+  const ESC_KEY_CODE = 'Escape';
+    const isEscKey = e.code === ESC_KEY_CODE;
+    if (isEscKey) {
+        this.close();
+    } 
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+    gallery: document.querySelector('.gallery'),
+}
+
+//генерація розмітки
+const galleryMarkup = createGalleryCardsMarkup(galleryItems);
+refs.gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryCardsMarkup(imgs) {
+    return imgs.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" onclick="event.preventDefault()" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`;
+    }).join('');
+};
+
+//Ініціалізація бібліотеки
+const lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250,});
